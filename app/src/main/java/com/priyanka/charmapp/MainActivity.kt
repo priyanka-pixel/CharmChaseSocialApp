7930d777e048d7c723ff11b39c1901651068f794
Priyanka Singh <59287277+priyanka-pixel@users.noreply.github.com>
2023-06-25 10:25:33 +0100

    Lottie animation

--- app/src/main/java/com/priyanka/charmapp/MainActivity.kt
+++ app/src/main/java/com/priyanka/charmapp/MainActivity.kt
@@ -1,22 +1,59 @@
 package com.priyanka.charmapp
 
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.cometchat.pro.core.AppSettings
+import com.cometchat.pro.core.CometChat
+import com.cometchat.pro.exceptions.CometChatException
 import com.priyanka.charmapp.ui.theme.CharmAppTheme
+import com.priyanka.charmchase.core.AppDetails
+import com.priyanka.charmchase.core.AppDetails.appID
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        //initialize with cometchat
+        val appSettings =
+            AppSettings.AppSettingsBuilder().subscribePresenceForAllUsers()
+                .setRegion(AppDetails.region)
+                .build()
+        CometChat.init(this, appID, appSettings, object : CometChat.CallbackListener<String>() {
+            override fun onSuccess(successMessage: String) {
+                Log.d(ContentValues.TAG, "Initialization completed successfully")
+                CometChat.setSource("ui-kit", "android", "kotlin")
+            }
+
+            override fun onError(e: CometChatException) {
+                Log.d(ContentValues.TAG, "Initialization failed with exception: " + e.message)
+            }
+        })
 
 
         setContent {
@@ -26,7 +63,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SplashScreen(navController = NavController(this))
+
                 }
             }
         }
@@ -34,14 +72,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun SplashScreen(navController: NavController) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.symbol) )
+    val context = LocalContext.current
+    CharmAppTheme  {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .background(Color.Black),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        )
+        {
+            LottieAnimation(composition = composition, modifier = Modifier.size(400.dp)
+                .background(Color.Black),
+            isPlaying = true,
+            restartOnPlay = true,
+            iterations = 5)
+                Button(
+                    onClick = {
+                    },
+                    shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier
+                        .width(160.dp)
+                        .padding(top = 30.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CharmAppTheme {
-        Greeting("Android")
+                ) {
+                    Text(text = "LOGIN", color = Color.Red)
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(
+                    onClick = {
+
+                    },
+                    shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier
+                        .width(160.dp)
+                        .padding(top = 30.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+                ) {
+                    Text(text = "SIGNUP", color = Color.Red)
+                }
+            }
+
+        }
+
     }
-}
+
