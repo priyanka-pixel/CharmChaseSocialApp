15c1524b33f80dcecfd5fde63bbb9eba732bb7a1
Priyanka Singh <59287277+priyanka-pixel@users.noreply.github.com>
2023-06-27 11:54:07 +0100

    Update AppDetails.kt

--- app/src/main/java/com/priyanka/charmapp/core/AppDetails.kt
+++ app/src/main/java/com/priyanka/charmapp/core/AppDetails.kt
@@ -1,13 +1,9 @@
 package com.priyanka.charmchase.core
 
 object AppDetails {
-//    const val appID = "19828392834edf10"
-//    const val authKEY = "e71efffd36c9b78224d9a8becb7d3b120cc0eb34"
-//    const val region = "eu"
-//    const val apiKey = "5bec77a505ec96d8ec356688b09d77f1e6fb0ed6"
 
-    const val appID = "2403669a2834ba7f"
-    const val authKEY = "8c8f216e2182846639542ca0563073875aeb850a"
+    const val appID = ""
+    const val authKEY = ""
     const val region = "eu"
-    const val apiKey = "1cdc64d11d875623c0c53b58151a8939dfebb584"
+    const val apiKey = ""
 }
